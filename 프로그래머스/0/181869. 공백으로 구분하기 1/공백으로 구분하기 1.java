0a76c90c96ea10479e06e492e73cbbb2e180234a
여준서 <148769872+duwnstj@users.noreply.github.com>
2024-07-11 09:19:26 +0900

    Update 공백으로 구분하기 1.java
    주석문 추가

--- 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -2,5 +2,9 @@ class Solution {
     public String[] solution(String my_string) {
         
         return my_string.trim().split("\\s+");
+
+            //.trim()메서드를 통해서 앞뒤 공백을 제거하고, .split("\\s+")를 사용하여 
+        스페이스바,탭,엔터키등 공백처리가 되는 모든 것들을 기준으로 나눠서 배열로 나오게 만든다.
+        
     }
 }
