b6532bdf930c84b6fbc72d69c89db8b5f024bb83
여준서 <148769872+duwnstj@users.noreply.github.com>
2024-07-11 18:40:29 +0900

    Update 수 조작하기 1.java

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -14,7 +14,7 @@ class Solution {
                 n-=10;
             }
 //단일 문자는 .equals()메서드를 사용할 수 없고 ==을 사용하여 c와 control의 요소를 비교한다.
-//charAt()메서드를 통해 control문자열의 각 인덱스 값을 가져온다.
+//charAt()메서드를 통해 control문자열의 각 인덱스 값을 가져온다. 
         }
         return n;
     }
