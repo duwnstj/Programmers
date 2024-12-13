6dfb65e1e9cfa82691f769e59e6f343cb259735b
여준서 <148769872+duwnstj@users.noreply.github.com>
2024-12-13 09:56:08 +0900

    [Bronze III] Title: 최소, 최대, Time: 452 ms, Memory: 88304 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,29 +1,25 @@
 import java.io.*;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out))) {
 
-        int N = Integer.parseInt(bufferedReader.readLine());
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int N = Integer.parseInt(bufferedReader.readLine());
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
 
-        int[] numbers = new int[N];
-        int min = 0;
-        int max = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = Integer.parseInt(stringTokenizer.nextToken());
+            int min = Integer.MAX_VALUE;
+            int max = Integer.MIN_VALUE;
+
+            for (int i = 0; i < N; i++) {
+                int number = Integer.parseInt(stringTokenizer.nextToken());
+                min = Math.min(min, number);
+                max = Math.max(max, number);
+            }
+
+            bufferedWriter.write(min + " " + max);
+            bufferedWriter.flush();
         }
-        Arrays.sort(numbers);
-        min = numbers[0];
-        max = numbers[N - 1];
-        bufferedWriter.write(min + " " + max);
-        bufferedWriter.flush();
-
-
     }
-
 }
-
